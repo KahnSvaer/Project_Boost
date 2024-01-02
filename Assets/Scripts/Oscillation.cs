0abcb4483eed03af8b0aeb7f519eec1fe96d5671
Shivansh Pachnanda <Shivansh.pachnanda1@gmail.com>
2024-01-03 05:13:15 +0530

    Added moving Obstacles script and Application Quit Script

--- Assets/Scripts/Oscillation.cs
+++ Assets/Scripts/Oscillation.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Oscillation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float periodTime;
+    float movementFactor = 0;
+
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (periodTime <= Mathf.Epsilon) {return;}
+        float frequency = 2 * Mathf.PI / periodTime;
+        float rawSin = Mathf.Sin(frequency * Time.time);
+        movementFactor = (rawSin + 1)/2;
+        Vector3 offset = movementVector * movementFactor;
+        Vector3 newPosition = startingPosition + offset;
+        transform.position = newPosition;
     }
 }
